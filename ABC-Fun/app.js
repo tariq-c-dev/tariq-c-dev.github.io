13fc9042ba151fe487433e69bd04418affe0694b
طارق <136328512+tariq-c-dev@users.noreply.github.com>
2024-08-18 23:02:49 -0600

    nouvelle fonctionnalité partiel en l'arabe

--- ABC-Fun/app.js
+++ ABC-Fun/app.js
@@ -2,10 +2,116 @@ const languageSelect = document.getElementById('language-select');
 
 
 
+function jouerLeSon(event) {
+//window.speechSynthesis.cancel()
+
+console.log(event.target.id);
+const button = document.getElementById(event.target.id);
+
+const utterance = new SpeechSynthesisUtterance(event.target.id);
+utterance.lang = 'ar'; // Set the language to English (US)
+button.classList.add('move-up');
+setTimeout(() => {
+    button.classList.remove('move-up');
+}, 500);
+
+utterance.onstart = () => {
+    loadingIcon.style.display = 'none';
+};
+fois=fois+1
+
+window.speechSynthesis.speak(utterance);
+
+}
+
+function checkOrientation() {
+    // Check if the device is in portrait mode
+    if (window.orientation === 0 || window.orientation === 180) {
+        alert("دور الموبايل");
+    }
+}
+
+    function activerLarabe() {
+        // Define the HTML content to be copied
+        const contentToCopy = `
+        <div class="letter-row">
+<button class="styled-button" id="ت">ت</button>
+<button class="styled-button" id="ب">ب</button>
+<button class="styled-button" id="ا">ا</button>
+        </div>
+        
+        <div class="letter-row">
+
+<button class="styled-button" id="ح">ح</button>
+<button class="styled-button" id="ج">ج</button>
+<button class="styled-button" id="ث">ث</button>
+
+        </div>
+        
+        <div class="letter-row">
+
+<button class="styled-button" id="ذ">ذ</button>
+<button class="styled-button" id="د">د</button>
+<button class="styled-button" id="خ">خ</button>
+        </div>
+                <div class="letter-row">
+
+<button class="styled-button" id="س">س</button>
+<button class="styled-button" id="ز">ز</button>
+<button class="styled-button" id="ر">ر</button>
+        </div>
+
+        <div class="letter-row">
+        <button class="styled-button" id="ض">ض</button>
+        <button class="styled-button" id="ص">ص</button>
+
+        <button class="styled-button" id="ش">ش</button>
+        </div>
+        
+        <div class="letter-row">
+
+        <button class="styled-button" id="ع">ع</button>
+        <button class="styled-button" id="ظ">ظ</button>
+                <button class="styled-button" id="ط">ط</button>
+
+        </div>
+
+        <div class="letter-row">
+
+        <button class="styled-button" id="ق">ق</button>
+        <button class="styled-button" id="ف">ف</button>
+        <button class="styled-button" id="غ">غ</button>
+        </div>
+        
+                <div class="letter-row">
+        <button class="styled-button" id="م">م</button>
+        <button class="styled-button" id="ل">ل</button>
+        <button class="styled-button" id="ك">ك</button>
+    </div>
+
+                <div class="letter-row">
+        <button class="styled-button" id="و">و</button>
+        <button class="styled-button" id="ه">ه</button>
+        <button class="styled-button" id="ن">ن</button>
+
+    </div>
+            <div class="letter-row">
+
+            <button class="styled-button" id="ي">ي</button>
+    </div>
+        `;
+
+        // Insert the copied content into the buttonContainer
+        const buttonContainer = document.getElementById('buttonContainer');
+        buttonContainer.innerHTML = contentToCopy;
+    }
+
+
 let en = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 let es = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 let fr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "À", "Â", "Æ", "Ç", "É", "È", "Ê", "Ë", "Î", "Ï", "Ô", "Ù", "Û", "Ü"]
 let de = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Ö', 'Ü', 'ß'];
+let ar = [ "خ", "ح", "ج", "ث", "ت", "ب", "ا", "ص", "ش", "س", "ز", "ر", "ذ", "د", "ق", "ف", "غ", "ع", "ظ", "ط", "ض", "ي", "و", "ه", "ن", "م", "ل", "ك", ];
 
 let currentLanguage = (navigator.language || navigator.userLanguage).slice(0, 2);
 console.log(currentLanguage);
@@ -13,6 +119,7 @@ const loadingIcon = document.getElementById('loading');
 fois=0
 
 function decideLang() {
+window.removeEventListener("orientationchange", checkOrientation);
 fois=0
 console.log(fois)
 if (currentLanguage == "en") {
@@ -23,6 +130,9 @@ alphabet=fr;
 alphabet=es;
 } else if (currentLanguage == "de") {
 alphabet=de;
+} else if (currentLanguage == "ar") {
+alphabet=ar;
+window.addEventListener("orientationchange", checkOrientation);
 } else {
 alert("Device language unsupported. Default to English.");
 currentLanguage="en";
@@ -35,9 +145,21 @@ decideLang()
 languageSelect.addEventListener('change', () => {
 if (languageSelect.value != "select") {
 	currentLanguage = languageSelect.value;
-	decideLang()
+
+    if (currentLanguage == "ar") {
+    console.log("activation du mode arabe (de droit à gauche)")
+    activerLarabe()
+    const buttons = document.querySelectorAll('.container button');
+buttons.forEach(button => {
+console.log('hi')
+console.log(button)
+    button.addEventListener('click', jouerLeSon);
+});
+    } else {
 	removeLetters()
+	decideLang()
 	packLetters()
+	}
 	}
 });
 
@@ -59,7 +181,6 @@ alphabet.forEach(string => {
 packLetters()
 
 
-
 // Function to play sound
 function playSound(key) {
 window.speechSynthesis.cancel()
@@ -71,7 +192,7 @@ setTimeout(() => {
 }, 500);
 
 const utterance = new SpeechSynthesisUtterance(key.toLowerCase());
-console.log(fois)
+
 if (fois==0) {
 loadingIcon.style.display = 'block';
 }
